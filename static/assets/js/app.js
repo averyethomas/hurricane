d92c9bb0c583326a5894bb9644edcfd78f1dea8a
averyethomas <averyethomas@gmail.com>
2017-11-28 11:28:40 -0500

    added costs chart

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -69,3 +69,135 @@ app.controller('namedStormCtrl', ['$scope', function ($scope) {
 
 }]);
 
+app.controller('mapCtrl', ['$scope', function($scope){
+
+    $scope.apiKey = 'AIzaSyDMFwELhIzYsoRFQbX143HdGA2G4ZKENY4';
+
+}]);
+
+
+app.controller('costsCtrl', ['$scope', function($scope){
+
+    $scope.data = [
+        { year: 1980, eventNames: ['Allen'], costs: [1.9] },
+        { year: 1981, eventNames: [], costs: [0] },
+        { year: 1982, eventNames: [], costs: [0] },
+        { year: 1983, eventNames: ['Alicia'], costs: [7.5] },
+        { year: 1984, eventNames: [], costs: [0] },
+        { year: 1985, eventNames: ['Elena', 'Gloria', 'Juan'], costs: [3, 2, 3.5] },
+        { year: 1986, eventNames: [], costs: [0] },
+        { year: 1987, eventNames: [], costs: [0] },
+        { year: 1988, eventNames: [], costs: [0] },
+        { year: 1989, eventNames: ['Hugo'], costs: [18.3] },
+        { year: 1990, eventNames: [], costs: [0] },
+        { year: 1991, eventNames: ['Bob'], costs: [2.7] },
+        { year: 1992, eventNames: ['Andrew', 'Iniki'], costs: [48.1, 5.5] },
+        { year: 1993, eventNames: [], costs: [0] },
+        { year: 1994, eventNames: ['TS Alberto'], costs: [1.7] },
+        { year: 1995, eventNames: ['Marilyn', 'Opal'], costs: [3.4, 7.6] },
+        { year: 1996, eventNames: ['Fran'], costs: [8] },
+        { year: 1997, eventNames: [], costs: [0] },
+        { year: 1998, eventNames: ['Bonnie', 'Georges'], costs: [1.5, 9.1] },
+        { year: 1999, eventNames: ['Floyd'], costs: [9.7] },
+        { year: 2000, eventNames: [], costs: [0] },
+        { year: 2001, eventNames: ['TS Allison'], costs: [11.9] },
+        { year: 2002, eventNames: ['Lili'], costs: [1.5] },
+        { year: 2003, eventNames: ['Isabel'], costs: [7.4] },
+        { year: 2004, eventNames: ['Charley', 'Frances', 'Ivan','Jeanne'], costs: [21.3, 13, 27.3, 10] },
+        { year: 2005, eventNames: ['Dennis', 'Katrina', 'Rita', 'Wilma'], costs: [3.2, 161.3, 23.9, 24.5] },
+        { year: 2006, eventNames: [], costs: [0] },
+        { year: 2007, eventNames: [], costs: [0] },
+        { year: 2008, eventNames: ['Dolly', 'Gustav', 'Ike'], costs: [1.5, 7, 34.8] },
+        { year: 2009, eventNames: [], costs: [0] },
+        { year: 2010, eventNames: [], costs: [0] },
+        { year: 2011, eventNames: ['Irene', 'TS Lee'], costs: [15, 2.8] },
+        { year: 2012, eventNames: ['Isaac', 'Sandy'], costs: [3, 70.2] },
+        { year: 2013, eventNames: [], costs: [0] },
+        { year: 2014, eventNames: [], costs: [0] },
+        { year: 2015, eventNames: [], costs: [0] },
+        { year: 2016, eventNames: ['Matthew'], costs: [10.4] },
+        { year: 2017, eventNames: ['Harvey', 'Irma', 'Maria'], costs: [0, 0, 0] }
+    ];
+
+    $scope.years = [];
+    $scope.stormNumber = [];
+    $scope.stormCosts = [];
+
+    angular.forEach($scope.data, function(value, index){
+        //Push Each Year
+        $scope.years.push(value.year);
+        //Push Total Number of Storms
+        $scope.stormNumber.push(value.eventNames.length);
+        //Find Total Based on Costs Array
+        $scope.total = value.costs.reduce((x, y) => x + y);
+        $scope.stormCosts.push($scope.total);
+    });
+
+    Highcharts.chart('costs', {
+        chart: {
+            zoomType: 'xy'
+        },
+        title: {
+            text: 'Billion Dollar Storms By Year'
+        },
+        colors: ['#556A73', '#153959'],
+        xAxis: [{
+            categories: $scope.years,
+            crosshair: true
+        }],
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: '${value} B',
+                style: {
+                    color: '#153959'
+                }
+            },
+            title: {
+                text: 'Costs (In Billions)',
+                style: {
+                    color: '#153959'
+                }
+            }
+        }, { // Secondary yAxis
+            title: {
+                text: 'Number of Storms',
+                style: {
+                    color: '#556A73'
+                }
+            },
+            labels: {
+                format: '{value}',
+                style: {
+                    color: '#556A73'
+                }
+            },
+            opposite: true
+        }],
+        tooltip: {
+            shared: true
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            x: 120,
+            verticalAlign: 'top',
+            y: 100,
+            floating: true
+        },
+        series: [{
+            name: 'Total Storms',
+            type: 'column',
+            yAxis: 1,
+            data: $scope.stormNumber
+
+        }, {
+            name: 'Total Costs',
+            type: 'spline',
+            data: $scope.stormCosts,
+            tooltip: {
+                valuePrefix: '$',
+                valueSuffix: ' B'
+            }
+        }]
+    });
+ }]);
